453f0f47355a37384b3e6dde6157301ba48c081f
Pragati Chaudhary <savita845822@gmail.com>
2026-02-22 20:28:06 +0530

    file changed

--- js/client.js
+++ js/client.js
@@ -1,32 +1,39 @@
-const socket = io('http://localhost:8000');
+document.addEventListener("DOMContentLoaded", () => {
+    const socket = io('http://localhost:8000');
+    const form = document.getElementById('send-container');
+    const messageInput = document.getElementById('messageInp');
+    const messageContainer = document.querySelector('.container');
+const audio = new Audio('ting.mp3');
+    let name = prompt("Enter your name to join");
+    if (!name || name.trim() === "") name = "Anonymous";
+    socket.emit('user-joined', name);
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const message = messageInput.value.trim();
+        if(message === "") return;
+        append(`You: ${message}`, 'right');
+        socket.emit('send', message);
+        messageInput.value = '';
+    });
+    socket.on('receive', data => {
+        append(`${data.name}: ${data.message}`, 'left');
+        audio.play();
+    });
+    socket.on('user-joined', name => {
+        append(`${name} joined the chat`, 'left');
+        audio.play();
+    });
+    socket.on('left', name => {
+        append(`${name} left the chat`, 'left');
+        audio.play();
+    });
+    function append(message, position) {
+        const messageElement = document.createElement('div');
+        messageElement.innerText = message;
+        messageElement.classList.add('message', position);
+        messageContainer.append(messageElement);
+        // auto scroll down 🔥
+        messageContainer.scrollTop = messageContainer.scrollHeight;
+    }
 
-const form = document.getElementById('send-container');
-const messageInput = document.getElementById('messageInp');
-const messageContainer = document.querySelector('.container');
-
-const name = prompt("Enter your name to join");
-socket.emit('user-joined', name);
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const message = messageInput.value;
-
-    append(`You: ${message}`, 'right');
-    socket.emit('send', message);
-
-    messageInput.value = '';
 });
-socket.on('receive', data => {
-    append(`${data.name}: ${data.message}`, 'left');
-});
-socket.on('user-joined', name => {
-    append(`${name} joined the chat`, 'left');
-});
-socket.on('left', name => {
-    append(`${name} left the chat`, 'left');
-});
-function append(message, position) {
-    const messageElement = document.createElement('div');
-    messageElement.innerText = message;
-    messageElement.classList.add('message', position);
-    messageContainer.append(messageElement);
-}
